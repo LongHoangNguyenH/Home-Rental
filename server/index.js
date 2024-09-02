06ae18d4911a30d659a48c71bd1414b49557fb70
LongHoangNguyenH <longnh.uit@gmail.com>
2024-09-02 17:21:01 +0700

    define model

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
-const cors = require("cors")
+const cors = require('cors');
 
 app.use(cors())
 app.use(express.json())
